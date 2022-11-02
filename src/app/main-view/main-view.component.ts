6f2714f8f2d1cd8dc4364f10a11eab8f87ac1191
Do Xuan Dung <dungdx@pmr.vn>
2022-11-02 17:42:27 +0700

    call api async

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Board } from 'src/app/models/board.model';
 import { IBoard } from 'src/app/models/board.model';
 import { Column } from 'src/app/models/column.model';
 import { WorkspaceService } from '../service/workspace.service';
 import { TicketService } from '../service/ticket.service';
+import { TaskService } from '../service/task.service';
+import { map, mergeMap } from 'rxjs/operators';
+import { interval, of } from 'rxjs';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.scss'],
-  providers: [WorkspaceService, TicketService],
+  providers: [WorkspaceService, TicketService, TaskService],
 })
 export class MainViewComponent implements OnInit {
   isEditing: boolean = false;
   curTaskId: number = 0;
   lstWorkSpace: any[] = [];
   lstTicket: any[] = [];
+  lstTask: any[] = [];
   isVisibleDetail: boolean = false;
 
   constructor(
     private workspaceService: WorkspaceService,
-    private ticketService: TicketService
+    private ticketService: TicketService,
+    private taskService: TaskService
   ) {}
 
   board: Board = new Board('Test Board', [
@@ -95,18 +104,81 @@ export class MainViewComponent implements OnInit {
   }
 
   getAllTicketByWS() {
-    const id = '635a4342d33b9b9992200bd0';
-    this.ticketService.getAllTicketByWS(id).subscribe(
+    const workspaceId = '635a4342d33b9b9992200bd0';
+    // this.ticketService
+    //   .getAllTicketByWS(workspaceId)
+    //   .pipe(
+    //     map((data: any) => {
+    //       let tickets = [];
+    //       data.forEach((element) => {
+    //         tickets.push(element.tickets);
+    //       });
+    //       return of(tickets);
+    //     }),
+    //     mergeMap((data: any) =>
+    //       this.getAllTaskByTicket(data);
+    //     )
+    //   )
+    //   .subscribe(
+    //     (res: any) => {
+    //       console.log(res);
+    //       if (res !== null) {
+    //         console.log(res);
+
+    //         this.lstTicket = res[0].tickets;
+    //         console.log('lstTicket', this.lstTicket);
+    //         // for (let i = 0; i < this.lstTicket.length; i++) {
+    //         //   this.taskService
+    //         //     .getAllTaskByTicket(this.lstTicket[i].id)
+    //         //     .subscribe(
+    //         //       (res: any) => {
+    //         //         if (res !== null) {
+    //         //           this.lstTicket[i].push();
+    //         //         }
+    //         //       },
+    //         //       (error) => {}
+    //         //     );
+    //         // }
+    //         console.log('lstTicket', this.lstTicket);
+    //       }
+    //     },
+    //     (error) => {}
+    //   );
+
+    new Promise((resolve, reject) => {
+      this.ticketService.getAllTicketByWS(workspaceId).subscribe(
+        (res: any) => {
+          let tickets = [];
+          res.forEach(elm => {
+            tickets.push(elm.tickets)
+          })
+          console.log(tickets);
+          resolve(tickets);
+        },
+        (error) => {}
+      );
+    }).then((data: any[]) => {
+      console.log(data)
+      let tasks = [];
+      data.forEach(elm => {
+        this.taskService.getAllTaskByTicket(elm.id).subscribe(res => {
+          console.log(res);
+        })
+      })
+    });
+  }
+
+  getAllTaskByTicket(id) {
+    this.taskService.getAllTaskByTicket(id).subscribe(
       (res: any) => {
         if (res !== null) {
-          this.lstTicket = res[0].tickets;
-          console.log('lstTicket', this.lstTicket);
+          this.lstTask = res[0].tasks;
+          console.log('lstTask', this.lstTask);
         }
       },
       (error) => {}
     );
   }
-
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
